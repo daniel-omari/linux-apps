754d6f83764bdaf692ee600b76b9daf7a87c37d6
Daniel Omari <armageddontr2@gmail.com>
2026-02-09 21:30:27 +0000

    more nice stuff added hehe

--- distro-pack.sh
+++ distro-pack.sh
@@ -6,6 +6,18 @@ in="sudo dnf install -y"
 # Ubuntu / Debian / Kali
 #in="sudo apt install -y"
 
-$in spiderfoot # also sherlock, but manual instal is easier
+## Essentials
+$in python3
+$in python3-pip
+$in python3-virtualenv
+$in python3-devel
+$in gcc
+$in gcc-c++
+$in make
+$in gdb
+$in cmake
 $in git
 $in gzip
+
+## Cyber Security (Optionals)
+#$in spiderfoot # also sherlock, but manual instal is easier
--- flat-inst.sh
+++ flat-inst.sh
@@ -4,6 +4,7 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 fi="flatpak install flathub -y"
 
+## Essentials
 $fi io.mpv.Mpv # video player
 $fi org.gnome.Lollypop # music (spotify alternative)
 $fi org.geany.Geany # super light weight IDE
@@ -14,14 +15,15 @@ $fi io.github.shiftey.Desktop # Github Desktop
 $fi org.flameshot.Flameshot # Screenshot tool
 $fi com.visualstudio.code
 
-# Security / Privacy (Optionals)
+
+## Security / Privacy (Optionals)
 # $fi org.keepassxc.KeePassXC # Bitwarden but fully local (no accounts, no internet)
 # $fi org.cryptomator.Cryptomator
 # $fi com.protonvpn.www
 # $fi org.localsend.localsend_app # Send files locally securely
 # $fi org.torproject.torbrowser-launcher
 
-# Gaming (Optionals)
+## Gaming (Optionals)
 # $fi com.valvesoftware.Steam
 # $fi org.winehq.Wine
 # $fi com.github.Matoking.protontricks
