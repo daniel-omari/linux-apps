185626887bb3c80bf752da7dcd4daf2d07c9d9c6
Daniel Omari <armageddontr2@gmail.com>
2026-07-21 01:49:01 +0100

    few minor additions and tweaks

--- flat-inst.sh
+++ flat-inst.sh
@@ -5,24 +5,26 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 fi="flatpak install flathub -y"
 
 ## Essentials
-$fi io.mpv.Mpv # video player
+$fi org.videolan.VLC # video player
 $fi org.gnome.Lollypop # music (spotify alternative)
 $fi org.geany.Geany # super light weight IDE
 $fi org.kde.kwrite # notepad alternative
 $fi org.kde.filelight # shows disk usage and unused files
 $fi com.bitwarden.desktop
-$fi io.github.shiftey.Desktop # Github Desktop
 $fi org.flameshot.Flameshot # Screenshot tool
-$fi com.visualstudio.code
-$fi io.gitlab.librewolf-community # secure internet browser
+$fi com.brave.Browser
+
+# Programming (Optionals)
+# $fi io.github.shiftey.Desktop # Github Desktop
+# $fi com.visualstudio.code
 
 
 ## Security / Privacy (Optionals)
-# $fi org.keepassxc.KeePassXC # Bitwarden but fully local (no accounts, no internet)
 # $fi org.cryptomator.Cryptomator
-# $fi com.protonvpn.www
 # $fi org.localsend.localsend_app # Send files locally securely
-# $fi org.torproject.torbrowser-launcher
+$fi com.protonvpn.www
+$fi org.torproject.torbrowser-launcher
+$fi io.gitlab.librewolf-community # secure internet browser
 
 ## Gaming (Optionals)
 # $fi com.valvesoftware.Steam
